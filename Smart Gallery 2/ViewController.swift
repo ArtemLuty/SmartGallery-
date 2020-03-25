3d09c9b239784e4b532d21d72d01dfc1025d5057
Artem Kolesnikov <temofy@gmail.com>
2020-03-25 21:09:58 +0200

    Initial Commit

---
+++ Smart Gallery 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Gallery 2
+//
+//  Created by Корістувач on 25.03.2020.
+//  Copyright © 2020 kolesnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
